05358d5a3e76ab45491485332a11589e087d9e56
DharsanaSK <156123353+DharsanaSK@users.noreply.github.com>
2025-10-02 20:09:00 +0530

    changes committed

--- exp1/creational/singleton/Logger.java
+++ exp1/creational/singleton/Logger.java
@@ -2,12 +2,10 @@ package exp1.creational.singleton;
 
 import java.util.*;
 
-// Singleton Logger class
 public class Logger {
     private static Logger instance;
     private static List<String> logs;
 
-    // Private constructor
     private Logger() {
         logs = new ArrayList<>();
     }
@@ -19,14 +17,12 @@ public class Logger {
         }
         return instance;
     }
-
-    // Log a message
+   
     public void log(String message) {
         logs.add(message);
         System.out.println("[LOG]: " + message);
     }
 
-    // Get all logged messages
     public List<String> getLogs() {
         return logs;
     }
